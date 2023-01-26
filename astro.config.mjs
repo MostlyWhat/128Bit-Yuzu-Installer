f07ae9de2742b37d3595b0ea3a20f37a9f825c7f
MostlyWhat <mostlywhatstudios@gmail.com>
2023-01-26 21:32:43 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-moon-landing.netlify.app/",
+  site: 'https://mostlywhat.github.io',
+  base: '/128Bit-Yuzu-Installer',
   integrations: [
     tailwind(),
     image({
